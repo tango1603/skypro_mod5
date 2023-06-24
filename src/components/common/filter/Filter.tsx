e9d812aa1ef2ff3cbe0d6bba9ee8bab24d2b6851
Ilya Bashilov <tango1603@yandex.ru>
2023-06-24 17:22:25 +0400

    начал делать фильтр

--- src/components/common/filter/Filter.tsx
+++ src/components/common/filter/Filter.tsx
@@ -1,17 +1,35 @@
-import Button from "../button/Button";
+import { useState } from "react";
+import Button from "./buttons/Button";
 import "./Filter.scss";
 
-const dummyFilterAPI = [
-  { text: "исполнителю", type: "author" },
-  { text: "году выпуска", type: "year" },
-  { text: "жанру", type: "genre" },
-];
-
 const Filter = () => {
+  const [filterValue, setFilter] = useState("");
+
+  const onFilterClickHandler = (type: string) => {
+    setFilter(type);
+    console.log("onFilterClickHandle", type);
+  };
+
+  const filterAPI = [
+    {
+      key: 1,
+      text: "исполнителю",
+      type: "author",
+      onClick: onFilterClickHandler,
+    },
+    {
+      key: 2,
+      text: "году выпуска",
+      type: "year",
+      onClick: onFilterClickHandler,
+    },
+    { key: 3, text: "жанру", type: "genre", onClick: onFilterClickHandler },
+  ];
+
   return (
     <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
-      {dummyFilterAPI.map(Button)}
+      {filterAPI.map(Button)}
     </div>
   );
 };
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,36 +1,52 @@
 import logo from "../../img/logo.png";
 import "./Nav.scss";
 
+interface BurgerProps {
+  isOpen?: void;
+}
+
+export const BurgerButton = ({ isOpen }: BurgerProps) => {
+  return (
+    <div className="nav__burger burger">
+      <span className="burger__line"></span>
+      <span className="burger__line"></span>
+      <span className="burger__line"></span>
+    </div>
+  );
+};
+
+export const MenuList = () => {
+  return (
+    <div className="nav__menu menu">
+      <ul className="menu__list">
+        <li className="menu__item">
+          <a href="http://" className="menu__link">
+            Главное
+          </a>
+        </li>
+        <li className="menu__item">
+          <a href="http://" className="menu__link">
+            Мой плейлист
+          </a>
+        </li>
+        <li className="menu__item">
+          <a href="http://" className="menu__link">
+            Войти
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 const Nav = () => {
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
         <img className="logo__image" src={logo} alt="logo" />
       </div>
-      <div className="nav__burger burger">
-        <span className="burger__line"></span>
-        <span className="burger__line"></span>
-        <span className="burger__line"></span>
-      </div>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      <BurgerButton />
+      <MenuList />
     </nav>
   );
 };
